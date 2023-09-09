76b576fd310ccfb562b68c2fd43706c364c62976
Matheus Alves <mathalves07.winstore@outlook.com>
2023-09-09 16:17:27 -0300

    Finished the to-do app

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import styles from "./App.module.css";
+import React, { useState } from 'react';
+
+// components
+import Modal from "./components/Modal/Modal";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import TaskForm from "./components/TaskForm/TaskForm";
+import TaskList from "./components/TaskList/TaskList";
+
+// interfaces
+import { ITask } from "./interfaces/Task";
+
+function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (title: string): void => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.title !== title;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.getElementById("modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  };
+
+  return (
+    <div>
+      <Modal
+        title="Edit task"
+        children={
+          <TaskForm
+            btnText="Edit"
+            taskList={taskList}
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
+      />
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.todo_form}>
+          <h2>What's on your mind?</h2>
+          <TaskForm
+            taskList={taskList}
+            setTaskList={setTaskList}
+            btnText="Add new task"
+          />
+        </div>
+        <div className="todo-container">
+          <h2>Your tasks:</h2>
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,36 @@
+import styles from "./TaskList.module.css";
+import { ITask } from "../../interfaces/Task";
+
+interface Props {
+  taskList: ITask[];
+  handleDelete(title: string): void;
+  handleEdit(task: ITask): void;
+}
+
+const TaskList = ({ taskList, handleDelete, handleEdit }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task, index) => (
+          <div key={index} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Difficulty: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil" onClick={() => handleEdit(task)}></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => handleDelete(task.title)}
+              ></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>There are no tasks.</p>
+      )}
+    </>
+  );
+};
+
+export default TaskList;
